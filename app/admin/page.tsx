faffab2a430d89f87d23fe044355b90e2827a531
Harlloh <alloolorunfemi@gmail.com>
2024-01-14 22:18:28 +0100

    asdf

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -6,11 +6,17 @@ import getUsers from "@/actions/getUsers";
 import Container from "../components/Container";
 import BarGraph from "./BarGraph";
 import getGraphData from "@/actions/getGraphData";
+import NullData from "../components/NullData";
+import { getCurrentUser } from "@/actions/getCurrentUser";
 
 async function page() {
   const products = await getProducts({ category: null });
   const orders = await getOrders();
   const users = await getUsers();
+  const currentUser = await getCurrentUser();
+  if (!currentUser || currentUser.role !== "ADMIN") {
+    return <NullData title="Oops access denied" />;
+  }
 
   const graphData = await getGraphData();
 
